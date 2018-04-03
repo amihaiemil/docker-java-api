2490f43fd5c59c93431d6545292cf790ea6c8d3b
George Aristy <george.aristy@gmail.com>
2018-04-03 14:55:24 -0400

    (#72) New 'Inspection' class
    * Added new reusable 'Inspection' class for *.inspect() methods
    * Refactored RtSwarm.inspect and RtContainer.inspect accordingly
    * Added missing license header in RtContainer and PayloadOf

--- src/main/java/com/amihaiemil/docker/RtContainer.java
+++ src/main/java/com/amihaiemil/docker/RtContainer.java
@@ -1,12 +1,35 @@
+/**
+ * Copyright (c) 2018, Mihai Emil Andronache
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1)Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2)Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3)Neither the name of docker-java-api nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.amihaiemil.docker;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 
-import javax.json.Json;
 import javax.json.JsonObject;
 import java.io.IOException;
 import java.net.URI;
@@ -47,20 +70,7 @@ final class RtContainer implements Container {
 
     @Override
     public JsonObject inspect() throws IOException {
-        final HttpGet inspect = new HttpGet(this.baseUri.toString() + "/json");
-        final HttpResponse response = this.client.execute(inspect);
-        final int status = response.getStatusLine().getStatusCode();
-        final JsonObject info;
-        if(status == HttpStatus.SC_OK) {
-            info = Json
-                .createReader(response.getEntity().getContent()).readObject();
-        } else {
-            throw new UnexpectedResponseException(
-                inspect.getURI().toString(), status, HttpStatus.SC_OK
-            );
-        }
-        inspect.releaseConnection();
-        return info;
+        return new Inspection(this.client, this.baseUri.toString() + "/json");
     }
 
     @Override
