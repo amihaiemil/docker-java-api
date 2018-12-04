1e57be130a63eacf104ad272a8b7eb8f4b3ea00f
Mihai A <amihaiemil@gmail.com>
2018-12-04 11:41:59 +0200

    checkstyle

--- src/main/java/com/amihaiemil/docker/RemoteDocker.java
+++ src/main/java/com/amihaiemil/docker/RemoteDocker.java
@@ -39,7 +39,7 @@ import org.apache.http.client.HttpClient;
  * @checkstyle ParameterNumber (150 lines)
  * @todo #178:30min Based on the input from
  *  https://github.com/amihaiemil/docker-java-api/issues/178#issuecomment-444029856
- *  we should setup a "remote" Docker instance in Travis, for Integration testing.
+ *  we should setup a Docker instance in Travis, for Integration testing.
  */
 public final class RemoteDocker extends RtDocker {
 
