0427c8fd2f5d7a46c5cea39b9959f60f5ae59fbb
sbespalov <serge.bespalov@gmail.com>
2018-12-14 14:28:47 +0600

    JsonResource improvements

--- src/main/java/com/amihaiemil/docker/JsonResource.java
+++ src/main/java/com/amihaiemil/docker/JsonResource.java
@@ -73,137 +73,143 @@ abstract class JsonResource implements JsonObject {
     }
     
     @Override
-    public JsonArray getJsonArray(final String name) {
+    public final JsonArray getJsonArray(final String name) {
         return this.resource.getJsonArray(name);
     }
 
     @Override
-    public JsonObject getJsonObject(final String name) {
+    public final JsonObject getJsonObject(final String name) {
         return this.resource.getJsonObject(name);
     }
 
     @Override
-    public JsonNumber getJsonNumber(final String name) {
+    public final JsonNumber getJsonNumber(final String name) {
         return this.resource.getJsonNumber(name);
     }
 
     @Override
-    public JsonString getJsonString(final String name) {
+    public final JsonString getJsonString(final String name) {
         return this.resource.getJsonString(name);
     }
 
     @Override
-    public String getString(final String name) {
+    public final String getString(final String name) {
         return this.resource.getString(name);
     }
 
     @Override
-    public String getString(final String name, final String defaultValue) {
+    public final String getString(final String name, 
+        final String defaultValue
+    ) {
         return this.resource.getString(name, defaultValue);
     }
 
     @Override
-    public int getInt(final String name) {
+    public final int getInt(final String name) {
         return this.resource.getInt(name);
     }
 
     @Override
-    public int getInt(final String name, final int defaultValue) {
+    public final int getInt(final String name, final int defaultValue) {
         return this.resource.getInt(name, defaultValue);
     }
 
     @Override
-    public boolean getBoolean(final String name) {
+    public final boolean getBoolean(final String name) {
         return this.resource.getBoolean(name);
     }
 
     @Override
-    public boolean getBoolean(final String name, final boolean defaultValue) {
+    public final boolean getBoolean(final String name, 
+        final boolean defaultValue
+    ) {
         return this.resource.getBoolean(name, defaultValue);
     }
 
     @Override
-    public boolean isNull(final String name) {
+    public final boolean isNull(final String name) {
         return this.resource.isNull(name);
     }
 
     @Override
-    public ValueType getValueType() {
+    public final ValueType getValueType() {
         return this.resource.getValueType();
     }
 
     @Override
-    public int size() {
+    public final int size() {
         return this.resource.size();
     }
 
     @Override
-    public boolean isEmpty() {
+    public final boolean isEmpty() {
         return this.resource.isEmpty();
     }
 
     @Override
-    public boolean containsKey(final Object key) {
+    public final boolean containsKey(final Object key) {
         return this.resource.containsKey(key);
     }
 
     @Override
-    public boolean containsValue(final Object value) {
+    public final boolean containsValue(final Object value) {
         return this.resource.containsValue(value);
     }
 
     @Override
-    public JsonValue get(final Object key) {
+    public final JsonValue get(final Object key) {
         return this.resource.get(key);
     }
 
     @Override
-    public JsonValue put(final String key, final JsonValue value) {
+    public final JsonValue put(final String key, final JsonValue value) {
         return this.resource.put(key, value);
     }
 
     @Override
-    public JsonValue remove(final Object key) {
+    public final JsonValue remove(final Object key) {
         return this.resource.remove(key);
     }
 
     @Override
-    public void putAll(final Map<? extends String, ? extends JsonValue> map) {
+    public final void putAll(
+        final Map<? extends String, ? extends JsonValue> map
+    ) {
         this.resource.putAll(map);
     }
 
     @Override
-    public void clear() {
+    public final void clear() {
         this.resource.clear();
     }
 
     @Override
-    public Set<String> keySet() {
+    public final Set<String> keySet() {
         return this.resource.keySet();
     }
 
     @Override
-    public Collection<JsonValue> values() {
+    public final Collection<JsonValue> values() {
         return this.resource.values();
     }
 
     @Override
-    public Set<Entry<String, JsonValue>> entrySet() {
+    public final Set<Entry<String, JsonValue>> entrySet() {
         return this.resource.entrySet();
     }
 
     @Override
-    public String toString() {
+    public final String toString() {
         return this.resource.toString();
     }
     
     @Override
-    public boolean equals(final Object other) {
+    public final boolean equals(final Object other) {
         return this.resource.equals(other);
     }
 
     @Override
-    public int hashCode() {
+    public final int hashCode() {
         int hash = 5;
         hash = 89 * hash + Objects.hashCode(this.resource);
         return hash;
