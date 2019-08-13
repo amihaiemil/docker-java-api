9ed402f69c46cde03c3968ad2c69e094db11bd49
Marco Teixeira <marcoo.teixeira@gmail.com>
2019-08-13 19:36:37 -0300

    Adding pause and unpause features.

--- src/test/java/com/amihaiemil/docker/RtContainerITCase.java
+++ src/test/java/com/amihaiemil/docker/RtContainerITCase.java
@@ -169,7 +169,6 @@ public final class RtContainerITCase {
         ).containers().create("TestUnpause", this.containerJsonObject());
         container.start();
         container.pause();
-        
         MatcherAssert.assertThat(
                 this.pausedState(container),
                 new IsEqual<>(true)
